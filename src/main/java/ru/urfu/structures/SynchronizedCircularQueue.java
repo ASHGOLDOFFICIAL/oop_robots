568ab2956c8ea9894fd2264e8cb4bd35957c2506
ashgoldofficial <104313094+ASHGOLDOFFICIAL@users.noreply.github.com>
2025-03-18 02:17:23 +0500

    Реализовал синхронизированную цикличную очередь на массиве.

---
+++ src/main/java/ru/urfu/structures/SynchronizedCircularQueue.java
@@ -0,0 +1,119 @@
+package ru.urfu.structures;
+
+import java.util.AbstractQueue;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * <p>Circular queue with fixed size.</p>
+ *
+ * <p>Does not allow null elements.</p>
+ *
+ * <p>If collection is full, then when adding a new element,
+ * oldest one will be overwritten.</p>
+ *
+ * @param <E> the type of elements held in this deque
+ */
+public class SynchronizedCircularQueue<E> extends AbstractQueue<E> {
+    private final E[] elements;
+    private final int capacity;
+
+    private int size = 0;
+    private int start = 0;
+    private int end = 0;
+
+    /**
+     * <p>Constructor.</p>
+     *
+     * @param capacity queue capacity
+     */
+    @SuppressWarnings("unchecked")
+    public SynchronizedCircularQueue(int capacity) {
+        if (capacity <= 0) {
+            throw new IllegalArgumentException("Illegal Capacity: " + capacity);
+        }
+        this.capacity = capacity;
+        this.elements = (E[]) new Object[capacity];
+    }
+
+    @Override
+    public synchronized Iterator<E> iterator() {
+        return new SynchronizedCircularQueueIterator();
+    }
+
+    @Override
+    public synchronized int size() {
+        return size;
+    }
+
+    @Override
+    public synchronized boolean offer(E e) {
+        if (e == null) {
+            return false;
+        }
+
+        if (size == elements.length) {
+            start = (start + 1) % capacity;
+            --size;
+        }
+        elements[end] = e;
+        end = (end + 1) % capacity;
+        ++size;
+        return true;
+    }
+
+    @Override
+    public synchronized E poll() {
+        if (size == 0) {
+            return null;
+        }
+        final E head = peek();
+        elements[start] = null;
+        start = (start + 1) % capacity;
+        --size;
+        return head;
+    }
+
+    @Override
+    public synchronized E peek() {
+        return elements[start];
+    }
+
+    /**
+     * <p>Iterator for {@link SynchronizedCircularQueue}.</p>
+     */
+    private class SynchronizedCircularQueueIterator implements Iterator<E> {
+        /**
+         * Index of element to be returned by subsequent call to next.
+         */
+        int cursor;
+
+        /**
+         * Number of elements yet to be returned.
+         */
+        int remaining = size();
+
+        /**
+         * <p>Constructor.</p>
+         */
+        SynchronizedCircularQueueIterator() {
+            cursor = start;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return remaining > 0;
+        }
+
+        @Override
+        public E next() {
+            if (remaining <= 0) {
+                throw new NoSuchElementException();
+            }
+            final E e = elements[cursor];
+            cursor = (cursor + 1) % capacity;
+            remaining--;
+            return e;
+        }
+    }
+}
---
+++ src/test/java/ru/urfu/structures/SynchronizedCircularQueueTest.java
@@ -0,0 +1,93 @@
+package ru.urfu.structures;
+
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Тесты для {@link SynchronizedCircularQueue}.
+ */
+@SuppressWarnings("MagicNumber")
+class SynchronizedCircularQueueTest {
+    private SynchronizedCircularQueue<Integer> queue;
+
+    /**
+     * <p>Создаём коллекцию для проверки.</p>
+     */
+    @BeforeEach
+    void setup() {
+        queue = new SynchronizedCircularQueue<>(3);
+    }
+
+    /**
+     * <p>Проверяем, что:</p>
+     * <ul>
+     *     <li>Коллекция отказывается от предложения null.</li>
+     *     <li>Содержимое и размер не меняется после попытки.</li>
+     * </ul>
+     */
+    @Test
+    @DisplayName("Добавление null не проходит")
+    void testNullAddition() {
+        Assertions.assertFalse(queue.offer(null));
+        Assertions.assertIterableEquals(List.of(), queue);
+        Assertions.assertEquals(0, queue.size());
+    }
+
+    /**
+     * <p>Проверяем, что при добавлении элементов
+     * корректно меняется размер и содержимое коллекции.</p>
+     */
+    @Test
+    @DisplayName("Обычное добавление в коллекцию")
+    void testNormalAddition() {
+        queue.offer(1);
+        queue.offer(2);
+        Assertions.assertEquals(2, queue.size());
+        Assertions.assertIterableEquals(List.of(1, 2), queue);
+
+        queue.offer(3);
+        queue.offer(4);
+        Assertions.assertEquals(3, queue.size());
+        Assertions.assertIterableEquals(List.of(2, 3, 4), queue);
+    }
+
+    /**
+     * <p>Проверяем, что:</p>
+     * <ul>
+     *     <li>{@link SynchronizedCircularQueue#poll} на пустой коллекции возвращает null;</li>
+     *     <li>{@link SynchronizedCircularQueue#poll} возвращает корректный элемент;</li>
+     *     <li>{@link SynchronizedCircularQueue#poll} на непустой коллекции удаляет элемент из коллекции.</li>
+     * </ul>
+     */
+    @Test
+    @DisplayName("Обычное удаление из коллекции")
+    void testRemoval() {
+        Assertions.assertNull(queue.poll());
+
+        queue.add(5);
+        Assertions.assertEquals(5, queue.poll());
+        Assertions.assertEquals(0, queue.size());
+        Assertions.assertIterableEquals(List.of(), queue);
+
+        Assertions.assertNull(queue.poll());
+    }
+
+    /**
+     * <p>Проверяем, что:</p>
+     * <ul>
+     *     <li>{@link SynchronizedCircularQueue#peek} возвращает корректный элемент
+     *     и не меняет содержимое коллекции и её размер.</li>
+     * </ul>
+     */
+    @Test
+    @DisplayName("peek")
+    void testPeek() {
+        queue.add(1);
+        Assertions.assertEquals(1, queue.peek());
+        Assertions.assertEquals(1, queue.size());
+        Assertions.assertIterableEquals(List.of(1), queue);
+    }
+}
